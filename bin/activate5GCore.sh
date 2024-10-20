1d931b302c536474350744c4edd7baccfe8d77d2
Michael Tomer <Jester3141@gmail.com>
2024-10-19 22:42:56 -0600

    Fixed some startup conditions

--- bin/activate5GCore.sh
+++ bin/activate5GCore.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -ex
 
+# sudo ./deploy-srs.sh release_24_04 srsRAN_Project
+
 
 # create a network namespace for the UE
-sudo ip netns add ue1
-sudo ip netns add ue2
-sudo ip netns add ue3
-sudo ip netns add ue4
+sudo ip netns add ue1 || true
+sudo ip netns add ue2 || true
+sudo ip netns add ue3 || true
+sudo ip netns add ue4 || true
+
+# Double check that we have properly finished setup and building of the 5g core.
+# the easiest way to do this is to check for the presence of the docker executable
+if [ ! -f /usr/bin/docker ]; then
+    echo "Docker not installed.  This indicates the 5G core is not installed."
+    echo "You can install it with the following command:"
+    echo ""
+    echo "sudo ./deploy-srs.sh release_24_04 srsRAN_Project"
+    echo ""
+    exit 1
+fi
+
 
 # start 5gc container
 sudo docker compose -f /opt/srsRAN_Project/docker/docker-compose.yml up 5gc
--- bin/activateGoodGNodeB.sh
+++ bin/activateGoodGNodeB.sh
@@ -2,6 +2,20 @@
 set -ex
 
 
+# Double check that we have properly finished setup and building of the 5g core.
+# the easiest way to do this is to check for the presence of the docker executable
+if [ ! -f /usr/local/bin/gnb ]; then
+    echo "gnb not installed.  This indicates the gnb client is not installed yet"
+    echo "You can install it with the following command:"
+    echo ""
+    echo "sudo ./deploy-srs.sh release_23_11 srsRAN_4G"
+    echo ""
+    exit 1
+fi
+
+
+
+
 # copy the existing gnb conf file to a new one
 sudo cp /etc/srsran/gnb.conf /etc/srsran/gnb_good.conf
 
